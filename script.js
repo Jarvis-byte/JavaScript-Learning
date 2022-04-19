13bacf9de2279bf8c9772db59c665934acd43279
Arka <arkamazumder0@gmail.com>
2022-04-19 19:10:30 +0530

    If/Else

--- script.js
+++ script.js
@@ -134,3 +134,23 @@ lines");
 console.log(`String
 multiple
 lines`);
+////////////////////////////////////
+// Taking Decisions: if / else Statements
+const age = 15;
+
+if (age >= 18) {
+  console.log("Sarah can start driving license 🚗");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthyear = 2012;
+
+let century;
+if (birthyear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
